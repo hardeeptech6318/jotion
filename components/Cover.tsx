23bc995aa695d02c4b083f2ab4e99bc1711b4879
hardeepsinh parmar <hardeepsinhparmar2020@gmail.com>
2023-11-21 21:44:38 +0530

    search routing correction commit

--- components/Cover.tsx
+++ components/Cover.tsx
@@ -37,44 +37,83 @@ function Cover({url,preview}:CoverProps) {
         })
     }
 
-  return (
-    <div className={cn("relative w-full h-[35vh] group",
-    !url && "h-[12vh]",
-    url && "bg-muted"
-    )}>
-{!!url && (
-    <Image
-    src={url}
-    fill
-    alt="cover"
-    className="object-cover"
-    />
-)}
+    return (
+        <div className={cn(
+          "relative w-full h-[35vh] group",
+          !url && "h-[12vh]",
+          url && "bg-muted"
+        )}>
+          {!!url && (
+            <Image
+              src={url}
+              fill
+              alt="Cover"
+              className="object-cover"
+            />
+          )}
+          {url && !preview && (
+            <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
+              <Button
+                onClick={() => coverImage.onReplace(url)}
+                className="text-muted-foreground text-xs"
+                variant="outline"
+                size="sm"
+              >
+                <ImageIcon className="h-4 w-4 mr-2" />
+                Change cover
+              </Button>
+              <Button
+                onClick={onRemove}
+                className="text-muted-foreground text-xs"
+                variant="outline"
+                size="sm"
+              >
+                <X className="h-4 w-4 mr-2" />
+                Remove
+              </Button>
+            </div>
+          )}
+        </div>
+      )
 
-{url && !preview} && (
-    <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
-        <Button
-        onClick={()=>coverImage.onReplace(url || "")}
-        className="text-muted-foreground text-xs"
-        variant="outline"
-        size="sm"
-        >
-            <ImageIcon className="h-4 w-4 mr-2"/>
-            Change cover
-        </Button>
-        <Button
-        onClick={onRemove}
-        className="text-muted-foreground text-xs"
-        variant="outline"
-        size="sm"
-        >
-            <X className="h-4 w-4 mr-2"/>
-            Remove
-        </Button>
-    </div>
-)
-    </div>
-  )
+//   return (
+//     <div className={cn("relative w-full h-[35vh] group",
+//     !url && "h-[12vh]",
+//     url && "bg-muted"
+//     )}>
+// {!!url && (
+//     <Image
+//     src={url}
+//     fill
+//     alt="cover"
+//     className="object-cover"
+//     />
+// )}
+
+// {url && !preview} && (
+//     <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
+//         <Button
+//         onClick={()=>coverImage.onReplace(url || "")}
+//         className="text-muted-foreground text-xs"
+//         variant="outline"
+//         size="sm"
+//         >
+//             <ImageIcon className="h-4 w-4 mr-2"/>
+//             Change cover
+//         </Button>
+//         <Button
+//         onClick={onRemove}
+//         className="text-muted-foreground text-xs"
+//         variant="outline"
+//         size="sm"
+//         >
+//             <X className="h-4 w-4 mr-2"/>
+//             Remove
+//         </Button>
+//     </div>
+// )
+//     </div>
+//   )
 }
 
 export default Cover
